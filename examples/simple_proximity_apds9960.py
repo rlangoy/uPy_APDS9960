96d24812e07506c10bc6eae7e437b557adcf58ab
Rune Langøy <rune.langoy@gmail.com>
2019-12-26 20:32:29 +0100

    Doc Update

--- examples/simple_proximity_apds9960.py
+++ examples/simple_proximity_apds9960.py
@@ -2,10 +2,12 @@ import machine
 from time import sleep_ms
 from uPy_APDS9960.APDS9960 import APDS9960
 
+#Init I2C Buss
 i2c =  machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
-proxSensor=APDS9960(i2c, debug=True)
-proxSensor.enableProximitySensor()
+
+proxSensor=APDS9960(i2c)              # Enable sensor
+proxSensor.enableProximitySensor()    # Enable Proximit sensing
 
 while True:
         sleep_ms(25) # wait for readout to be ready
-        print(proxSensor.readProximity)
+        print(proxSensor.readProximity)   #Print the proximity value
