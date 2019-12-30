dc1dbbfa9a6778fde31180cad56cd2891565536e
Rune Langøy <rune.langoy@gmail.com>
2019-12-30 19:47:31 +0100

    Sample update

--- examples/simple_proximity_apds9960lite.py
+++ examples/simple_proximity_apds9960lite.py
@@ -9,14 +9,21 @@ apds9960=APDS9960LITE(i2c)
 apds9960.prox.enableProximity()
 apds9960.prox.setProximityInterruptThreshold(high=10,low=0,persistance=7)
 apds9960.prox.enableProximityInterrupt()
+apds9960.prox.eLEDCurrent=3     #100 mA (max)
+apds9960.prox.eProximityGain=3  #gain x8 (max)
+
+
+print("eProximityGain", apds9960.prox.eProximityGain)
+print("eLEDCurrent ",apds9960.prox.eLEDCurrent )
+
 
 ProxThPin=machine.Pin(0, machine.Pin.IN ,machine.Pin.PULL_UP)
 
 sleep_ms(50)
 
-while True:
-    sleep_ms(50)
-
-    if(ProxThPin.value()==0):
-        print("proximity:", apds9960.prox.readProximity() )
-        apds9960.prox.clearInterrupt()  #one more time
+#while True:
+#    sleep_ms(50)
+#
+#    if(ProxThPin.value()==0):
+#        print("proximity:", apds9960.prox.readProximity() )
+#        apds9960.prox.clearInterrupt()  #one more time
