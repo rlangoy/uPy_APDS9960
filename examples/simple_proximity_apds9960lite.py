166cef33e093650363ce4fabba3e0777bbcb975b
Rune Langøy <rune.langoy@gmail.com>
2019-12-30 16:12:24 +0100

    Update to PROX func

--- examples/simple_proximity_apds9960lite.py
+++ examples/simple_proximity_apds9960lite.py
@@ -5,10 +5,10 @@ from uPy_APDS9960.APDS9960LITE import APDS9960LITE
 i2c =  machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
 print("Lite APDS-9960 Proximity test ")
 
-proxSensor=APDS9960LITE(i2c)
-proxSensor.enableProximity()
-proxSensor.setProximityInterruptThreshold(high=10,low=0,persistance=7)
-proxSensor.enableProximityInterrupt()
+apds9960=APDS9960LITE(i2c)
+apds9960.prox.enableProximity()
+apds9960.prox.setProximityInterruptThreshold(high=10,low=0,persistance=7)
+apds9960.prox.enableProximityInterrupt()
 
 ProxThPin=machine.Pin(0, machine.Pin.IN ,machine.Pin.PULL_UP)
 
@@ -18,5 +18,5 @@ while True:
     sleep_ms(50)
 
     if(ProxThPin.value()==0):
-        print("proximity:", proxSensor.readProximity() )
-        proxSensor.clearInterrupt()  #one more time
+        print("proximity:", apds9960.prox.readProximity() )
+        apds9960.prox.clearInterrupt()  #one more time
