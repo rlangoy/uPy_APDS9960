171d43d51a1420d88f79762989f1e9a879de4ce2
Rune Langøy <rune.langoy@gmail.com>
2020-01-03 11:00:28 +0100

    doc Changed to Nature theme

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ default_role = "any"
 add_function_parentheses = True
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+#pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -80,26 +80,8 @@ todo_emit_warnings = True
 
 
 # -- Options for HTML output ----------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-#
-#if not on_rtd:  # only import and set the theme if we're building docs locally
-#    try:
-#        import sphinx_rtd_theme
-#        html_theme = 'bizstyle'
-#        html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), '.']
-#    except:
-#        html_theme = 'bizstyle'
-#        html_theme_path = ['.']
-#else:
-#    html_theme_path = ['.']
-
  
-html_theme = 'bizstyle'
+html_theme = 'nature'#'bizstyle'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
