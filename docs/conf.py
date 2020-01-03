3224cee624a367f92f9c5b1f934de9a3fc0a4f93
Rune Langøy <rune.langoy@gmail.com>
2020-01-03 10:19:59 +0100

    Changed doc style to sphinxjp.themes.bizstyle (https://pythonhosted.org/sphinxjp.themes.bizstyle/)

--- docs/conf.py
+++ docs/conf.py
@@ -89,10 +89,10 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     try:
         import sphinx_rtd_theme
-        html_theme = 'sphinx_rtd_theme'
+        html_theme = 'bizstyle'
         html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), '.']
     except:
-        html_theme = 'default'
+        html_theme = 'bizstyle'
         html_theme_path = ['.']
 else:
     html_theme_path = ['.']
